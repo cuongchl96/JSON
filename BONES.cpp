3362bb42253de3e2b622c4f0d7a999cc2b359ff3
Lương Tuấn Dũng <tuanluong04011996@gmail.com>
2016-05-22 23:30:37 +0700

    Create BONES.cpp

---
+++ BONES.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+#define FOR(i,x,y) for(int i = x; i <= y;i++)
+#define Fill(a,b) memset(a,b,sizeof(a))
+const int maxn = 100;
+using namespace std;
+
+int main(){
+    int i, j, k, d[maxn], kq, s1, s2, s3;
+    Fill(d,0);
+    scanf("%d%d%d",&s1,&s2,&s3);
+    FOR(i,1,s1)
+        FOR(j,1,s2)
+            FOR(k,1,s3) d[i+j+k]++;
+    kq = 0;
+    FOR(i,1,maxn)
+        if (d[i] > d[kq]) kq = i;
+    printf("%d",kq);
+}
+
